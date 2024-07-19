f21593cd0603b1bbb0300d910e185068c39c7585
Sonir Aster <sunwolf114@gmail.com>
2024-07-19 16:30:29 +0300

    changed header styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,8 +7,8 @@ const About = (props) => {
             <div className={styles.text}>
                 <h1 className={styles.title}>Who we are?</h1>
                 <p className={styles.description}>
-                    Citadel was founded as a circle of warriors with different talents, ideals and traditions following the same goal: <span>Create</span>, <span>Support</span>, <span>Improve</span>. 
-                    Citadel warrior's circle is the best 
+                    Citadel was founded as a circle of enthusiasts with different talents, ideals and traditions following the same goal: <span>Create</span>, <span>Support</span>, <span>Improve</span>. 
+                    Citadel ordo Acris is the best 
                     way to find the most effective approach to your business that will help you to rise up on higher level.
                     <i> We will go further where others could not.</i>
                 </p>
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from './Header';
 
 const HeaderContainer = (props) => {
-    useEffect(() => {
-        function splitLetters (title) {
-            const letters = title.innerHTML.split('');
-            title.innerHTML = '';
-            letters.map(letter => {
-                let span = document.createElement('span');
-                span.classList.add('char');
-                span.innerHTML = letter;
-                title.appendChild(span);
-                return span;
-            })
-        }
-        let logo = document.querySelector('#headerLogo');
-        splitLetters(logo);
-    }, [])
-
     return <Header
         showAbout={props.handleAboutClick}
         showContacts={props.handleContactClick}
