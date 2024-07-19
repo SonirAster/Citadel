aac5095e95b62598c27771a0cba35d2c6603c3e3
Sonir Aster <sunwolf114@gmail.com>
2024-07-19 16:57:39 +0300

    changes case

--- src/works.js
+++ src/works.js
@@ -1,5 +1,3 @@
-import note1 from './assets/works/notebook/note1.png';
-import note2 from './assets/works/notebook/note2.png';
 import knitting1 from './assets/works/knitting/1.png';
 import knitting2 from './assets/works/knitting/6.png';
 import knitting3 from './assets/works/knitting/14.png';
@@ -9,9 +7,13 @@ import srb2 from './assets/works/srb/15.png';
 import srb3 from './assets/works/srb/20.png';
 import srb4 from './assets/works/srb/3.png';
 import srb5 from './assets/works/srb/Screenshot_5.png';
+import ds1 from './assets/works/notebook/s1.png';
+import ds2 from './assets/works/notebook/s2.png';
+import ds3 from './assets/works/notebook/s3.png';
+import ds4 from './assets/works/notebook/s3.png';
 
 const banners = [
-    [note1, note2],
+    [ds3, ds2, ds1, ds4],
     [knitting1, knitting2, knitting3, knitting4],
     [srb1, srb2, srb3, srb4, srb5]
 ]
