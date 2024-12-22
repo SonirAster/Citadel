9544cfb3371ea0666ed3bbcc05b94a52832735c5
Sonir Aster <sunwolf114@gmail.com>
2024-12-22 15:52:27 +0200

    added map and kanban board

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React, { useRef } from 'react';
 import styles from './Header.module.css';
 import {NavLink} from 'react-router-dom';
+import item from '../../assets/name.png';
 
 const Header = (props) => {
     const menu = useRef(null);
 
     function openMenu () {
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflowY = 'hidden';
+        document.body.style.overflowX = 'hidden';
         menu.current.style.transform = 'translateY(0)';
     }
 
@@ -18,13 +20,15 @@ const Header = (props) => {
 
     return (
         <div className={styles.header}>
-            <NavLink to='/' className={styles.logo}>Citadel</NavLink>
+            <NavLink to='/' className={styles.logo}>
+                <img src={item} alt="" />
+            </NavLink>
             <div className={styles.menuWrapper}>
                 <button onClick={ openMenu } className={styles.menuIcon}>Menu</button>
                 <ul ref={menu} className={styles.menu}>
                     <button className={styles.closeIcon} onClick={ closeMenu }>+</button>
                     {props.navMenuList.map(item => (
-                        <li className={styles.menuItem}>
+                        <li className={styles.menuItem} key={item.id}>
                             <button onClick={() => {
                                 closeMenu();
                                 props.handleRef(item.ref);
--- src/works.js
+++ src/works.js
@@ -33,6 +33,15 @@ import p6_photo2 from '../src/assets/works/news_articles site/1.png';
 import p6_photo3 from '../src/assets/works/news_articles site/2.png';
 import p6_photo4 from '../src/assets/works/news_articles site/3.png';
 
+import p7_photo1 from '../src/assets/works/USA mapp app/map page.png';
+import p7_photo2 from '../src/assets/works/USA mapp app/page one.png';
+
+import p8_photo1 from '../src/assets/works/kanban board/s1.png';
+import p8_photo2 from '../src/assets/works/kanban board/s2.png';
+import p8_photo3 from '../src/assets/works/kanban board/s3.png';
+import p8_photo4 from '../src/assets/works/kanban board/s4.png';
+import p8_photo5 from '../src/assets/works/kanban board/s5.png';
+
 
 const state = {
     portfolio: [
@@ -42,6 +51,8 @@ const state = {
         {images: [p4_photo1, p4_photo2, p4_photo3, p4_photo4, p4_photo5, p4_photo6, p4_photo7, p4_photo8], title: 'Sweet Recipes Book', description: 'The main concept we chose to create such sites is as follows: to show the variety of your products and their large quantity in such a way that each item does not lose its originality in a row with other equally interesting and attractive products.'},
         {images: [p5_photo1, p5_photo2, p5_photo3, p5_photo4], title: 'Deep Sound', description: "We took part in the creation of this project. Here you can see our teamwork skills. We provide 'Fast-help' services in completing any project."},
         {images: [p6_photo1, p6_photo2, p6_photo3, p6_photo4], title: 'Coming Soon', description: "We are always on the move and we do not stand still. We are developing a concept for a news website. Only we can catch the style of your news site because we are oriented on the working accroding to the priciple 'Time - is money'. We understand better than others the specifics  of a news site, which must first of all be undestandable, so that each visitor to your news site stays with you and forever remembers the style we created specially for you."},
+        {images: [p7_photo1, p7_photo2], title: 'USA Routing App', description: 'Usefull app that will save your money and time! See gas stations on your way, gasoline prices and total price of your trip in USA. This app will help you to count money that would be spent on the fuel by choosing certain route.'},
+        {images: [p8_photo1, p8_photo2, p8_photo3, p8_photo4, p8_photo5], title: 'Kanban Board', description: "Time is money and that's true. In some situations you might need to have your business systemized. Need to optimize your work? There is nothing easier! You need a work management board that will  help and simplify the work with your orders, tasks and any processes of your business. Best offer: buy a website and get a Work Management tool with a 50% discount!"}
     ],
     services: [
         {'title': 'Logo',  'description': 'Recognisable exclusive logo to pass through the centuries.'}, 
